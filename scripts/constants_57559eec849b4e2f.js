2fe80873deef5b4595eedf8dc1f27bc329227691
jaxellis <67706810+jaxellis@users.noreply.github.com>
2025-10-13 06:19:05 -0700

    Shorten constants

--- scripts/constants_57559eec849b4e2f.js
+++ scripts/constants_57559eec849b4e2f.js
@@ -1,17 +1,7 @@
-const COLLAPSE_KEY = 'dune-awakening-checklist-collapsed-v1';
-const FILTERS_KEY = 'dune-awakening-checklist-filters-v1';
-const SETTINGS_KEY = 'dune-awakening-checklist-settings-v1';
-const STORAGE_KEY = 'dune-awakening-checklist-v1';
+export const COLLAPSE_KEY = 'dune-awakening-checklist-collapsed-v1';
+export const FILTERS_KEY = 'dune-awakening-checklist-filters-v1';
+export const SETTINGS_KEY = 'dune-awakening-checklist-settings-v1';
+export const STORAGE_KEY = 'dune-awakening-checklist-v1';
 
-const IMAGE_PATH_PREFIX = 'images/';
-
-const UNDO_STACK_MAX = 20;
-
-export {
-	COLLAPSE_KEY,
-	FILTERS_KEY,
-	SETTINGS_KEY,
-	STORAGE_KEY,
-	IMAGE_PATH_PREFIX,
-	UNDO_STACK_MAX,
-};
+export const IMAGE_PATH_PREFIX = 'images/';
+export const UNDO_STACK_MAX = 20;
